efc2958e9f989bf3b0ddf12147ca7c4b187512a4
Enrique Salazar <57001244+quiquesm@users.noreply.github.com>
2022-10-05 13:03:58 +0200

    Backend added

---
+++ forumhome-backend/src/main/java/com/forumHome/back/BackforumApplication.java
@@ -0,0 +1,13 @@
+package com.forumHome.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackforumApplication.class, args);
+	}
+
+}
---
+++ forumhome-backend/src/test/java/com/forumHome/back/BackforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forumHome.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
