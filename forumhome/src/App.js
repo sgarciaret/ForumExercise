9a3873e92653adc9cb7bf23c2d167117b8ac6d3c
Cristina <cristinaherrojo.10@gmail.com>
2022-10-06 16:07:28 +0200

    Threads

--- forumhome/src/App.js
+++ forumhome/src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewUser from './components/NewUser';
 import Login from './components/Login';
 import ForumHome from "./components/ForumHome";
-import Thread1 from "./components/threads/Thread1";
-import Thread2 from "./components/threads/Thread2";
-import Thread3 from "./components/threads/Thread3";
-import Thread4 from "./components/threads/Thread4";
+import Thread from "./components/threads/Thread";
 import CreatePostT from "./components/CreatePostT";
 import CreatePostBody from "./components/CreatePostBody";
 
 function App() {
+
+  const [threadId, setThreadId] = useState();
 
   const styleFooter = {
     backgroundColor: "#5D2B7E",
@@ -26,11 +26,8 @@ function App() {
 
         <Route index element={<Login />} />
         <Route path="signup" element={<NewUser />} />
-        <Route path="home" element={<ForumHome />} />
-        <Route path="thread1" element={<Thread1 />} />
-        <Route path="thread2" element={<Thread2 />} />
-        <Route path="thread3" element={<Thread3 />} />
-        <Route path="thread4" element={<Thread4 />} />
+        <Route path="home" element={<ForumHome onChangeThreadId={setThreadId}/>} />
+        <Route path="thread" element={<Thread threadId={threadId}/>} />
         <Route path="createPostT" element={<CreatePostT />} />
         <Route path="createPostBody" element={<CreatePostBody />} />
       </Routes>
--- forumhome/src/components/ForumHome.js
+++ forumhome/src/components/ForumHome.js
@@ -1,28 +1,9 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ForumHome.css";
 
-function ForumHome() {
-<<<<<<< HEAD
-    const styleBar = {
-        backgroundColor: "#5D2B7E",
-        width: "100%",
-        height: "10vh"
-      };
-
-    const [thread1, setThread1] = useState("Thread 1");
-    const [thread2, setThread2] = useState("Thread 2");
-    const [thread3, setThread3] = useState("Thread 3");
-    const [thread4, setThread4] = useState("Thread 4");
-
-  return (
-    <>
-    <div style={styleBar}>
-        <Link to="/home" className="decolink"><h1>Forum</h1></Link>
-      </div>
-      <div class="logoutbtn">
-=======
+function ForumHome( { onChangeThreadId }) {
     const [threads, setThreads] = useState([]);
     const styleLink = {
       textDecoration : "none",
@@ -40,7 +21,6 @@ function ForumHome() {
   return (
     <>
       <div className="logoutbtn">
->>>>>>> 27e76321f0a826d4967809f11d6d91ecd09d7f16
         <Link to="/">
           <button className="btn btn-secondary btn-sm me-md-2">Log out</button>
         </Link>
@@ -49,36 +29,9 @@ function ForumHome() {
         className="border border-gray border-4 rounded-3 p-5 mt-5 threads"
       >
         <h2>Threads</h2>
-<<<<<<< HEAD
-        <div
-          className="border border-green border-3 rounded-3 p-4 mt-4 w-80"
-        >
-            <Link to="/thread1" className="decolink"><h3>{thread1}</h3></Link>
-          
-          <p>Description Thread 1</p>
-        </div>
-        <div
-          className="border border-green border-3 rounded-3 p-4 mt-4 w-80"
-        >
-          <Link to="/thread2" className="decolink"><h3>{thread2}</h3></Link>
-          <p>Description Thread 2</p>
-        </div>
-        <div
-          className="border border-green border-3 rounded-3 p-4 mt-4 w-80"
-        >
-          <Link to="/thread3" className="decolink"><h3>{thread3}</h3></Link>
-          <p>Description Thread 3</p>
-        </div>
-        <div
-          className="border border-green border-3 rounded-3 p-4 mt-4 w-80"
-        >
-          <Link to="/thread4" className="decolink"><h3>{thread4}</h3></Link>
-          <p>Description Thread 4</p>
-        </div>
-=======
 
         {threads.map(thread=>(
-          <Link to="/" style={styleLink}>
+          <Link to="/thread" style={styleLink} onClick={() => onChangeThreadId(thread.id)}>
             <div
               className="border border-green border-3 rounded-3 p-4 mt-4 w-80"
             >
@@ -87,7 +40,6 @@ function ForumHome() {
             </div>
           </Link>
         ))}
->>>>>>> 27e76321f0a826d4967809f11d6d91ecd09d7f16
       </div>
     </>
   );
