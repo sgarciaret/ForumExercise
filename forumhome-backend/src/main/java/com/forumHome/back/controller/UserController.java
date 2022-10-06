d3137f8403477fa787be5708c7ce1e739c96fbcd
Enrique Salazar <57001244+quiquesm@users.noreply.github.com>
2022-10-06 11:31:21 +0200

    Backend login usuario

--- forumhome-backend/src/main/java/com/forumHome/back/controller/UserController.java
+++ forumhome-backend/src/main/java/com/forumHome/back/controller/UserController.java
@@ -3,6 +3,7 @@ package com.forumHome.back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,24 +12,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.forumHome.back.model.User;
+import com.forumHome.back.repository.UserRepository;
 import com.forumHome.back.service.UserService;
-
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin
 public class UserController {
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserRepository userRepo;
 
-    @PostMapping("/add")
-    public String add(@RequestBody User user){
-    	userService.saveUser(user);
-        return "New user is added";
-    }
+	@PostMapping("/add")
+	public String add(@RequestBody User user) {
+		userService.saveUser(user);
+		return "New user is added";
+	}
 
-    @GetMapping("/getAll")
-    public List<User> list(){
-        return userService.getAllUsers();
-    }
+	@GetMapping("/getAll")
+	public List<User> list() {
+		return userService.getAllUsers();
+	}
+
+	@GetMapping("/users")
+	public String listUsers(Model model) {
+		List<User> listUsers = userRepo.findAll();
+		model.addAttribute("listUsers", listUsers);
+
+		return "users";
+	}
 }
---
+++ forumhome-backend/src/main/java/com/forumHome/back/model/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.forumHome.back.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+	private User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUserName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+	
+}
